0d50ae0396b5cd24daf880dca17642319c48b07d
James Drake <oppa_us@yahoo.com>
2025-05-01 16:57:15 -0700

    A tree builder that doesn't crash
    It looks OK, but trying to confirm through visualization. Kinda hard to
    build that with escape characters, so working on that ...

--- main.c
+++ main.c
@@ -5,10 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: do we need all 256 values?
-#define MAXVAL 256
+#define MAXVAL 127
+
+// TAB: 9, NEWLINE: 10
+enum { TAB = '\t', NEWLINE = '\n', SPACE = ' ', DEL = '\x7f' };
 
 struct node {
+  int id;
   int count;
   char value;
   struct node *left;
@@ -26,27 +29,48 @@ int frqcmp(const void *a, const void *b) {
 }
 
 void printArray(struct node arr[], size_t size) {
-  for (size_t i = 0; i < size; ++i)
-    printf("%d ", arr[i].count);
+  for (size_t i = 32; i < size; ++i) {
+    printf("[%c] %d,", arr[i].value, arr[i].count);
+    if (i % 10 == 0)
+      printf("\n");
+  }
   printf("\n");
 }
 
 void printTree(struct node *tree) {
-  if (tree != NULL && isprint(tree->value)) {
+  if (tree != NULL) {
     printTree(tree->left);
-    printf("%c %8d\n", tree->value, tree->count);
+    if (isprint(tree->value))
+      printf("%c %8d\n", tree->value, tree->count);
     printTree(tree->right);
   }
 }
 
+void dotTree(struct node *tree) {
+  char subg[] = "%c->{%c,%c};";
+  char attr[] = "%c[label='%c\\n%i'];";
+  if (tree != NULL) {
+    dotTree(tree->left);
+
+    if (tree->left && tree->right)
+      printf(subg, tree->count, tree->left->count, tree->right->count);
+
+    if (isprint(tree->value))
+      printf(attr, tree->value, tree->value, tree->count);
+
+    dotTree(tree->right);
+  }
+}
+
+void graphTree(struct node *tree) {
+  printf("digraph {");
+  dotTree(tree);
+  printf("}\n");
+}
+
 int main(int argc, char **argv) {
 
-  if (argc < 2) {
-    fprintf(stderr, "Usage: compress <filename>\n");
-    abort();
-  }
-
-  char *file = argv[1];
+  char *file = "test.txt";
 
   // NOTE: no error handling (at least for now)
   FILE *fp = fopen(file, "r");
@@ -56,11 +80,12 @@ int main(int argc, char **argv) {
   size_t size = sizeof(freq) / sizeof(freq[0]);
 
   // tracks pointers into the frequency table while creating sorted trees
-  // holds the binary tree
+  // will hold the binary tree
   struct node *lut[MAXVAL];
 
   // initialize frequency table
   for (int i = 0; i < MAXVAL; i++) {
+    freq[i].id = i + 1;
     freq[i].value = (char)i;
     freq[i].count = 0;
     freq[i].left = NULL;
@@ -69,30 +94,29 @@ int main(int argc, char **argv) {
   }
 
   int ch;
-  while ((ch = getc(fp)) != EOF) {
-    freq[ch].count++;
-  }
+  while ((ch = getc(fp)) != EOF)
+    if (ch < 127)
+      freq[ch].count++;
 
   // smallest frequencies to largest
+  // NOTE: the lut tracks with this, i.e., no need to sort the lut
   qsort(freq, size, sizeof(struct node), frqcmp);
+
   printArray(freq, size);
 
-  for (int i = 0; i < size; i++)
-    printf("%d ", lut[i]->count);
-
-  printf("\n");
+  //  TODO: This works, but I'm not quite sure how yet
+  //
+  // printArray(*lut, size);
 
   // allocate storage for internal (non-leaf) nodes
   struct node internals[MAXVAL - 1];
   struct node *next = &internals[0];
 
-  // start lut at the second node because we combine the first two elements
-  struct node *p = lut[1];
-
   // convert to a binary tree
   int idx = 0;
-  while (idx < MAXVAL) {
-    // pop (well shift) off the next two minimum values
+  while (idx < MAXVAL - 1) {
+    // get the next two minimum values, lut must be kept in 'priority' order
+    // by min(count)
     struct node *n1 = lut[idx];
     struct node *n2 = lut[idx + 1];
 
@@ -105,9 +129,6 @@ int main(int argc, char **argv) {
     new->right = n2;
     new->value = 0;
 
-    if (new->count > 0)
-      printf("Adding node with count: %d\n", new->count);
-
     // Need to save this new node in a way that it can be compared and assigned
     // later ... in the right order. Try a sort of stack-based operation using
     // the lut
@@ -115,11 +136,28 @@ int main(int argc, char **argv) {
     // slot for the new node based on count comparisons, shifting pointers left
     // to make room.
     int slot = ++idx;
-    while (slot < MAXVAL && lut[slot++]->count < new->count)
-      lut[slot - 1] = lut[slot];
+
+    // we're at the end this must be equal to, or larger than the last count
+    if (idx == MAXVAL - 1) {
+      lut[slot] = new;
+      break;
+    }
+
+    // Replace n2 with the new (tree) node, if next element is larger
+    if (new->count < lut[slot + 1]->count) {
+      lut[slot] = new;
+      continue;
+    }
+
+    // shift nodes left until a spot is found, mind the boundary
+    do {
+      lut[slot] = lut[slot + 1];
+      slot++;
+      if (slot == MAXVAL - 1)
+        break;
+    } while (lut[slot]->count < new->count);
 
     lut[slot] = new;
   }
-
-  printTree(lut[idx]);
+  // graphTree(lut[idx]);
 }
